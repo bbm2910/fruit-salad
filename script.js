4ee5c82b004dedfe5ebd554abee8f9ccce1b6b35
bobby2910 <b.mastacan@gmail.com>
2025-03-27 16:22:22 +0000

    first task

--- script.js
+++ script.js
@@ -17,39 +17,49 @@ async function fetchFruitData(fruit) {
     const response = await fetch(
       `https://fruit-api-5v0j.onrender.com/fruits/${fruit}`
     );
+    const responseImage = await fetch(
+      `https://pixabay.com/api/?key=49560513-12d40ed63b48b95ec83f288e3&q=${fruit}+fruit`
+    );
+
     const data = await response.json();
-    addFruit(data);
-  } catch {}
+    const imageData = await responseImage.json();
+    addFruit(data, imageData);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // function fetchFruitData(fruit) {
-//   fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
-//     .then(processResponse)
-//     .then((data) => addFruit(data))
-//     .catch((err) => console.log(err));
+//     fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
+//         .then(processResponse)
+//         .then((data) => addFruit(data))
+//         .catch((err) => console.log(err));
 
-//   fetch(
-//     `https://pixabay.com/api/?key=49560481-dd50fb5b150dea4d442d11dba&q=${fruit}+fruit`
-//   )
-//     .then((respomse) => respomse.json())
-//     .then((data) => console.log(data))
-//     .catch((err) => console.log(err));
+//     fetch(`https://pixabay.com/api/?key=49560513-12d40ed63b48b95ec83f288e3&q=${fruit}+fruit`)
+//         .then((response) => response.json())
+//         .then((data) => console.log(data))
+//         .catch((err) => console.log(err))
 // }
 
 function processResponse(response) {
   if (response.ok) {
     return response.json();
   } else {
-    throw "Error!!! - status code: " + response.status;
+    throw "Error - status code: " + response.status;
   }
 }
 
-function addFruit(fruit) {
+function addFruit(fruit, imageData) {
   console.log(fruit);
-  const li = document.createElement("li");
-  li.textContent = fruit.name;
-  li.addEventListener("click", removeFruit, { once: true });
-  fruitList.appendChild(li);
+  console.log(imageData);
+  //const li = document.createElement("li");
+  const img = document.createElement("img");
+  img.src = imageData.hits[0].previewURL;
+  //li.textContent = fruit.name;
+  //li.addEventListener("click", removeFruit, { once: true });
+  img.addEventListener("click", removeFruit, { once: true });
+  //fruitList.appendChild(li);
+  fruitList.appendChild(img);
 
   calories += fruit.nutritions.calories;
   // console.log("calorie infor", calories);
